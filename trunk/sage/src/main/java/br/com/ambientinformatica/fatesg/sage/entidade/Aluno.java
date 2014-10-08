9479fc9802b1e4876d0239a4ae1b06d148f960d8
jonathancarvalho01 <jonathancarvalho01@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2014-10-08 01:22:35 +0000

    Entidade Aluno provisória.

---
+++ trunk/sage/src/main/java/br/com/ambientinformatica/fatesg/sage/entidade/Aluno.java
@@ -0,0 +1,65 @@
+package br.com.ambientinformatica.fatesg.sage.entidade;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Aluno {
+	
+	@Id
+	@GeneratedValue(generator = "aluno_seq", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "aluno_seq", sequenceName = "aluno_seq", allocationSize = 1, initialValue = 1)
+	private Integer id;
+	
+	private String nome;
+	
+	private String email;
+	
+	private String telefone;
+	
+	private String celular;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+}
