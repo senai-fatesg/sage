f2551e340b10f14653ca1c8932733de4f530041b
hellin <hellin@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2014-11-08 18:43:34 +0000

    vagadeestagio

--- trunk/sage/src/main/java/br/com/ambientinformatica/fatesg/sage/persistencia/VagaEstagioDao.java
+++ trunk/sage/src/main/java/br/com/ambientinformatica/fatesg/sage/persistencia/VagaEstagioDao.java
@@ -1,8 +1,13 @@
 package br.com.ambientinformatica.fatesg.sage.persistencia;
 
+import java.util.List;
+
 import br.com.ambientinformatica.jpa.persistencia.Persistencia;
+import br.com.ambientinformatica.fatesg.sage.entidade.Empresa;
 import br.com.ambientinformatica.fatesg.sage.entidade.VagaEstagio;
 
 public interface VagaEstagioDao extends Persistencia<VagaEstagio> {
+
+	List<VagaEstagio> listarPorEmpresa(Empresa empresa);
 
 }
--- trunk/sage/src/main/java/br/com/ambientinformatica/fatesg/sage/persistencia/VagaEstagioDaoJpa.java
+++ trunk/sage/src/main/java/br/com/ambientinformatica/fatesg/sage/persistencia/VagaEstagioDaoJpa.java
@@ -1,13 +1,35 @@
 package br.com.ambientinformatica.fatesg.sage.persistencia;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
 
+import br.com.ambientinformatica.fatesg.sage.entidade.Empresa;
+import br.com.ambientinformatica.fatesg.sage.entidade.Estagio;
 import br.com.ambientinformatica.fatesg.sage.entidade.VagaEstagio;
 import br.com.ambientinformatica.jpa.persistencia.PersistenciaJpa;
 
 @Repository("vagaEstagioDao")
 public class VagaEstagioDaoJpa extends PersistenciaJpa<VagaEstagio> implements
-		VagaEstagioDao {
+      VagaEstagioDao {
 	private static final long serialVersionUID = 1L;
+
+	@Override
+	public List<VagaEstagio> listarPorEmpresa(Empresa empresa) {
+		try {
+			String jpql = "select v from VagaEstagio v left join fetch v.empresa e where e = :empresa";
+			Query query = em.createQuery(jpql);
+			query.setParameter("empresa", empresa);
+			return (List<VagaEstagio>) query.getResultList();
+		} catch (NoResultException nre) {
+			return new ArrayList<VagaEstagio>();
+		} catch (Exception e) {
+			return new ArrayList<VagaEstagio>();
+		}
+	}
 
 }
